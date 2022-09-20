a250929a7d3b7a0880b819b70454bdd99e96b05c
FernandoCelmer <email@fernandocelmer.com>
2022-09-20 03:02:26 -0300

    No Task - Colored text printing on the terminal

---
+++ core/utils/text.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+
+class Colors:
+    VIOLETT = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def color(text: str, types: list):
+    types_sancii = ''
+    for item in types:
+        types_sancii = types_sancii + getattr(Colors, item.upper())
+
+    return reduce(lambda x, y: x + y, types_sancii) + text + Colors.ENDC
