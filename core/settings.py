6eb237768f1c95d5344fd5fb922b9faef539803e
FernandoCelmer <email@fernandocelmer.com>
2022-09-20 23:13:50 -0300

    No Task - Change settings

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,9 @@
-FOLDER_CONFIG = '.config'
-FOLDER_PROFILE = '.config/profiles'
-FOLDER_MODULE = '.config/modules'
+FOLDER_CONFIG = './.config'
+FOLDER_PROFILE = './.config/profiles'
+FOLDER_MODULE = './.config/modules'
 
-FILE_CONFIG = '.config/linux_config'
-FILE_PROFILE = '.config/linux_profile'
+FILE_CONFIG = './.config/linux_config'
+FILE_PROFILE = './.config/linux_profile'
 
-FILE_DISTRO = '.config/.os-release'
-FILE_SYSTEM = '.config/.hostnamectl'
+FILE_DISTRO = './.config/.os-release'
+FILE_SYSTEM = './.config/.hostnamectl'
