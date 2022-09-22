219b6fd523e60b311244de32ac031a98e6fa5845
FernandoCelmer <email@fernandocelmer.com>
2022-09-21 23:43:40 -0300

    No Task - Scripts update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-echo 'export PATH=$PATH":$HOME/linuxp"' >> ~/.profile
+git clone https://github.com/MyLinuxProfile/linux-profile-basic.git ~/linuxp --branch master
+
+echo 'export PATH=$PATH":$HOME/linuxp"' >> ~/.bashrc
