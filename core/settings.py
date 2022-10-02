a9819def03f6f9faef22b54f231ef106a387abf0
FernandoCelmer <email@fernandocelmer.com>
2022-10-02 16:58:32 -0300

    No Task - Adjusting the initial settings

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,23 @@
-FOLDER_CONFIG = './.config'
-FOLDER_PROFILE = './.config/profiles'
-FOLDER_MODULE = './.config/modules'
-FOLDER_LOG = './.config/logs'
+from json import load
+import os
 
-FILE_CONFIG = './.config/linux_config.json'
-FILE_PROFILE = './.config/linux_profile.json'
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
 
-FILE_DISTRO = './.config/.os-release'
-FILE_SYSTEM = './.config/.hostnamectl'
+PATH = os.getcwd()
+APP_NAME = os.environ.get('APP_NAME', 'linuxp')
+CONFIG = '.config' 
+
+FOLDER_CONFIG = f"{PATH}/{APP_NAME}/{CONFIG}"
+FOLDER_PROFILE = f'{FOLDER_CONFIG}/profiles'
+FOLDER_MODULE = f'{FOLDER_CONFIG}/modules'
+FOLDER_LOG = f'{FOLDER_CONFIG}/logs'
+
+FILE_CONFIG = f'{FOLDER_CONFIG}/linux_config.json'
+FILE_PROFILE = f'{FOLDER_CONFIG}/linux_profile.json'
+
+FILE_DISTRO = f'{FOLDER_CONFIG}/.os-release'
+FILE_SYSTEM = f'{FOLDER_CONFIG}/.hostnamectl'
