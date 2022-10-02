1d02524d97ed72c00e7800984199fb906074359a
FernandoCelmer <email@fernandocelmer.com>
2022-10-02 14:24:00 -0300

    ISSUE-#1 - Include of the logs for a exceptions

--- core/error.py
+++ core/error.py
@@ -1,63 +1,83 @@
 from core.utils.text import color
+from core.base.log import Log
+
+LOG_APP = Log().run_app()
 
 
 class ErrorLoadSettings(Exception):
 
-    def __init__(self,
-                 message="It is not possible to load the basic settings."):
+    def __init__(self):
+        message = "It is not possible to load the basic settings."
+        LOG_APP.error(message)
         super(ErrorLoadSettings, self).__init__(message)
 
 
 class ErrorParameterIsMissing(Exception):
 
     def __init__(self, parameter):
-        super(ErrorParameterIsMissing, self).__init__(
-            "Parameter --{} is missing".format(parameter))
+        message = f"Parameter --{parameter} is missing"
+        LOG_APP.error(message)
+        super(ErrorParameterIsMissing, self).__init__(message)
 
 
 class ErrorInvalidValue(Exception):
 
     def __init__(self, parameter):
-        super(ErrorInvalidValue, self).__init__(
-            "Parameter --{} invalid value".format(parameter))
+        message = f"Parameter --{parameter} invalid value"
+        LOG_APP.error(message)
+        super(ErrorInvalidValue, self).__init__(message)
 
 
-def print_error_settings():
+def print_error_settings(error):
+    message = "It is not possible to load the basic settings."
+    LOG_APP.error(f"{message} [{str(error)}]")
     print(
         color(
-            text="ERROR: It is not possible to load the basic settings.",
+            text="ERROR: " + message,
             types=['bold', 'red']
         )
     )
 
-def print_estrange_error():
+def print_error_estrange(error):
+    message = "Some weird error happened, but that's ok."
+    LOG_APP.error(f"{message} [{str(error)}]")
+
     print(
         color(
-            text="WARNING: Some weird error happened, but that's ok.",
+            text="ERROR: " + message,
             types=['bold', 'yellow']
         )
     )
 
 def print_parameter_is_missing(parameter):
+    message = f"Parameter --{parameter} is missing"
+    LOG_APP.warning(message)
+
     print(
         color(
-            text="WARNING: Parameter --{} is missing".format(parameter),
+            text=message.format("WARNING: "+parameter),
             types=['bold', 'yellow']
         )
     )
 
 def print_option_is_missing(parameter):
+    message = f"Option [{parameter}] is missing"
+    LOG_APP.warning(message)
+
     print(
         color(
-            text="WARNING: Option [{}] is missing".format(parameter),
+            text=message.format("WARNING: "+parameter),
             types=['bold', 'yellow']
         )
     )
 
 def print_error_invalid_value(parameter):
+    message = f"Parameter --{parameter} invalid value"
+    LOG_APP.warning(message)
+
     print(
         color(
-            text="WARNING: Parameter --{} invalid value".format(parameter),
+            text=message.format("WARNING: "+parameter),
             types=['bold', 'yellow']
         )
     )
