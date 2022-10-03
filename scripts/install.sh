e266c29d297b7eb9a0a11569968362ec812daf9e
FernandoCelmer <email@fernandocelmer.com>
2022-10-02 23:45:53 -0300

    No Task - Update Installation Script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Download Github Repository.
 git clone https://github.com/MyLinuxProfile/linux-profile-basic.git ~/linuxp --branch master
 
+# Creating new line in '.bashrc' file with project configuration.
 echo 'export PATH=$PATH":$HOME/linuxp"' >> ~/.bashrc
+
+# Exporting project configuration
+export PATH=$PATH":$HOME/linuxp"
