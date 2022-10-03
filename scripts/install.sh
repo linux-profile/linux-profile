e38caa23fbbb60fc8c554cf6ac265033f8788c50
FernandoCelmer <email@fernandocelmer.com>
2022-10-03 19:47:04 -0300

    No Task - Update Installation Script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Download Github Repository.
+BEGIN='\033[94m\033[1m'
+END='\033[0m'
+
+echo "$BEGIN 1 -$END Download Github Repository"
 git clone https://github.com/MyLinuxProfile/linux-profile-basic.git ~/linuxp --branch master
 
-# Creating new line in '.bashrc' file with project configuration.
+echo "$BEGIN 2 -$END Creating new line in '.bashrc' file with project configuration."
 echo 'export PATH=$PATH":$HOME/linuxp"' >> ~/.bashrc
 
-# Exporting project configuration
+echo "$BEGIN 3 -$END Exporting project configuration."
 export PATH=$PATH":$HOME/linuxp"
