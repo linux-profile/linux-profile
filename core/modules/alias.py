7d90efa021df7e4eebab58547580dfc562b200c3
FernandoCelmer <email@fernandocelmer.com>
2022-10-09 00:57:12 -0300

    ⚙️  NO-TASK - Adjust execution of 'alias' install commands

--- core/modules/alias.py
+++ core/modules/alias.py
@@ -1,14 +1,29 @@
+from os import system
 from pathlib import Path
 
 from core.base.system import System
-from core.utils.file import write_file
+from core.utils.file import read_file, get_content
 
 
 class SystemAlias(System):
 
     def setup_exec(self):
-        write_file(
-            content=f'\nalias {self.command}="{self.content}"',
-            path_file=str(Path.home()) + '/.bash_aliases',
-            mode='a'
-        )
+        path_file = str(Path.home()) + '/.bash_aliases'
+
+        file_system = read_file(path_file=path_file)
+        body = get_content(path_file=file_system, separator="=")
+
+        current_alias = f'\nalias {self.command}="{self.body}"'
+
+        rebase_alias = list()
+        for item in body:
+            if item[5:len(item)] != self.command:
+                new_alias = f'\nalias {item[5:len(item)]}="{body.get(item)}"'
+                rebase_alias.append(new_alias)
+    
+        system(f"true > {path_file}")
+        for _rebase in rebase_alias:
+            if _rebase != '\n':
+                system(f"echo '{_rebase}' >> {path_file}")
+
+        system(f"echo '{current_alias}' >> {path_file}")
