fd8aec4a9fb0c8826b409fab08708e9db46f4273
FernandoCelmer <email@fernandocelmer.com>
2022-10-16 18:23:57 -0300

    🛡  TEST - Update

---
+++ tests/settings.py
@@ -0,0 +1,21 @@
+PATH = './tests'
+
+PATH_CONFIG = f"{PATH}/.config"
+PATH_LINUXP = f"{PATH_CONFIG}/linuxp"
+PATH_PROFILE = f'{PATH_LINUXP}/profiles'
+PATH_LOG = f'{PATH_LINUXP}/logs'
+
+folder_location = {
+    "config": PATH_CONFIG,
+    "linuxp": PATH_LINUXP,
+    "profile": PATH_PROFILE,
+    "log": PATH_LOG,
+    "temp": "/tmp/linuxp/"
+}
+
+file_location = {
+    "config": f'{PATH_LINUXP}/linux_config.json',
+    "profile": f'{PATH_LINUXP}/linux_profile.json',
+    "log_app": f"{PATH_LOG}/app.log",
+    "bash_aliases": f'{PATH}/.bash_aliases'
+}
--- tests/test_base_config.py
+++ tests/test_base_config.py
@@ -1,57 +1,44 @@
-from os import path
 from shutil import rmtree
+from os import path, remove
 
 from linux_profile.base.config import BaseConfig
-from tests.settings import (
-    FILE_CONFIG,
-    FILE_PROFILE,
-    FOLDER_CONFIG,
-    FOLDER_MODULE,
-    FOLDER_PROFILE
-)
+from tests.settings import file_location, folder_location
 
 
 class Config(BaseConfig):
 
     def __init__(
             self,
-            module: str = None,
-            file_config: str = FILE_CONFIG,
-            file_profile: str = FILE_PROFILE,
-            folder_config: str = FOLDER_CONFIG,
-            folder_profile: str = FOLDER_PROFILE,
-            folder_module: str = FOLDER_MODULE):
-        self.module = module
-        self.file_config = file_config
-        self.file_profile = file_profile
-        self.folder_config = folder_config
-        self.folder_profile = folder_profile
-        self.folder_module = folder_module
+            _file: dict = file_location,
+            _folder: dict = folder_location,
+            **kwargs):
+        """
+        Structure that defines the main variables.
+        """
+        self.file = _file
+        self.folder = _folder
+
+        if kwargs:
+            for arg in kwargs:
+                setattr(self, arg, kwargs.get(arg))
+
 
 def test_set_folder():
     test_config = Config()
     test_config.set_folder()
 
-    assert path.isdir(FOLDER_CONFIG) == True
-    assert path.isdir(FOLDER_PROFILE) == True
-    assert path.isdir(FOLDER_MODULE) == True
+    assert path.isdir(folder_location.get("config")) == True
+    assert path.isdir(folder_location.get("linuxp")) == True
+    assert path.isdir(folder_location.get("profile")) == True
+    assert path.isdir(folder_location.get("log")) == True
+    assert path.isdir(folder_location.get("temp")) == True
 
-    rmtree(FOLDER_CONFIG)
+    rmtree(folder_location.get("config"))
 
 
-def test_add_config():
+def test_set_file():
     test_config = Config()
-    test_config.set_folder()
-    test_config.add_config()
+    test_config.set_file()
 
-    assert path.isfile("{}.json".format(FILE_CONFIG)) == True
-
-def test_load_config():
-    test_config = Config()
-    test_config.set_folder()
-    test_config.add_config()
-
-    test_config.load_config()
-
-    assert test_config.distro
-    assert test_config.system
+    assert path.isfile(file_location.get("bash_aliases")) == True
+    remove(file_location.get("bash_aliases"))
