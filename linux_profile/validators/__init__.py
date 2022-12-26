3bb988a97f4f534c9c19bbdd91754c1c9be659e6
FernandoCelmer <email@fernandocelmer.com>
2022-12-26 19:32:24 -0300

    📌 ISSUE-#89 - Remove useless code from implementation of 'terminal'

--- linux_profile/validators/__init__.py
+++ linux_profile/validators/__init__.py
@@ -1,11 +1,9 @@
 __all__ = [
     'InputAddAlias',
     'InputAddPackage',
-    'InputAddScript',
-    'InputAddTerminal'
+    'InputAddScript'
 ]
 
 from linux_profile.validators.input_alias import InputAddAlias
 from linux_profile.validators.input_package import InputAddPackage
 from linux_profile.validators.input_script import InputAddScript
-from linux_profile.validators.input_terminal import InputAddTerminal
