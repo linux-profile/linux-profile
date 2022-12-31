0216a7e541154e51309ec7c3b1e52046482e15a6
FernandoCelmer <email@fernandocelmer.com>
2022-12-31 20:57:37 -0300

    ❤️️  TEST - Update

--- tests/core/base/test_base_file.py
+++ tests/core/base/test_base_file.py
@@ -1,5 +1,5 @@
 from os import path, remove
-from linux_profile.base.file import BaseFile
+from linux_profile.base.file import File
 from linux_profile.base.error import ErrorFile
 
 
@@ -7,7 +7,7 @@ file_path = "./tests/helpers"
 
 
 def test_base_file_read_success():
-    text = BaseFile()
+    text = File()
     result = text.read(path_file=f"{file_path}/read.txt")
     assert result == 'Unit Test LinuxProfile'
 
@@ -15,14 +15,14 @@ def test_base_file_read_success():
 def test_base_file_write_success():
     test_path = f"{file_path}/write.txt"
 
-    text = BaseFile()
+    text = File()
     text.write(content="xpto", path_file=test_path)
     assert path.exists(test_path) == True
     remove(test_path)
 
 
 def test_base_file_read_lines_success():
-    text = BaseFile()
+    text = File()
     result = text.read_lines(path_file=f"{file_path}/read_lines.txt")
     assert len(result) == 10
 
@@ -30,14 +30,14 @@ def test_base_file_read_lines_success():
 def test_base_file_write_lines_success():
     test_path = f"{file_path}/write_lines.txt"
 
-    text = BaseFile()
+    text = File()
     text.write_lines(content=["xpto"], path_file=test_path)
     assert path.exists(test_path) == True
     remove(test_path)
 
 
 def test_base_file_read_error():
-    text = BaseFile()
+    text = File()
     try:
         text.read(path_file=f"{file_path}/xpto.txt")
     except Exception as error:
@@ -45,7 +45,7 @@ def test_base_file_read_error():
 
 
 def test_base_file_read_lines_error():
-    text = BaseFile()
+    text = File()
     try:
         text.read_lines(path_file=f"{file_path}/xpto.txt")
     except Exception as error:
@@ -55,7 +55,7 @@ def test_base_file_read_lines_error():
 def test_base_file_touch_success():
     test_path = f"{file_path}/touch.json"
 
-    text = BaseFile()
+    text = File()
     text.touch(test_path)
     assert path.exists(test_path) == True
     remove(test_path)
--- tests/core/base/test_base_file_action.py
+++ tests/core/base/test_base_file_action.py
@@ -1,6 +1,6 @@
 from json import loads
 from os import path, remove
-from linux_profile.base.file import BaseAction, BaseFile
+from linux_profile.base.file import BaseAction, File
 
 
 file_path = "./tests/helpers/database.json"
@@ -12,7 +12,7 @@ def test_base_file_action_create():
     content_create = {"id": 1, "name": "Linux"}
 
     action._create_item(content=content_create, module="script", tag="init")
-    content = loads(BaseFile.read(path_file=file_path))
+    content = loads(File.read(path_file=file_path))
 
     assert content["script"]["init"][1] == content_create
     action._delete_item(value=1)
@@ -22,26 +22,26 @@ def test_base_file_action_update():
     id = "EE959D7607694622B030BEEA1174AF56"
     action = BaseAction(file_path)
 
-    content = loads(BaseFile.read(path_file=file_path))
+    content = loads(File.read(path_file=file_path))
     assert content["script"]["init"][0]["name"] == "echo"
 
     action._update_item(content={"name": "linux"}, value=id)
-    content = loads(BaseFile.read(path_file=file_path))
+    content = loads(File.read(path_file=file_path))
     assert content["script"]["init"][0]["name"] == "linux"
 
     action._update_item(content={"name": "echo"}, value=id)
-    content = loads(BaseFile.read(path_file=file_path))
+    content = loads(File.read(path_file=file_path))
     assert content["script"]["init"][0]["name"] == "echo"
 
 
 def test_base_file_action_delete():
     action = BaseAction(file_path)
 
-    initial_content = loads(BaseFile.read(path_file=file_path))
+    initial_content = loads(File.read(path_file=file_path))
     assert len(initial_content["package"]["python"]) == 1
 
     action._delete_item(value="A48D2542AF3045888DC1242EF77C7666")
-    final_ontent = loads(BaseFile.read(path_file=file_path))
+    final_ontent = loads(File.read(path_file=file_path))
     assert len(final_ontent["package"]["python"]) == 0
 
     action._create_item(
