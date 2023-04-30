69fc22cd3fe6cae0e19276c7ee3ee30bffadd7f4
FernandoCelmer <email@fernandocelmer.com>
2023-04-30 15:24:37 -0300

    📌 ISSUE-#152 - Include option to save default text editor information

--- linux_profile/commands/config.py
+++ linux_profile/commands/config.py
@@ -2,6 +2,7 @@ import urllib.request
 
 from linux_profile.validators import InputConfig
 from linux_profile.base.settings import Settings
+from linux_profile.base.file import File
 
 
 class Config(Settings):
@@ -11,12 +12,22 @@ class Config(Settings):
         time the class is instantiated.
         """
         self.fields = InputConfig(**{
-            "url": self.url}
+            "url": self.url,
+            "editor": self.editor}
         )
 
         if self.fields.url:
             self.setup_url()
 
+        if self.fields.editor:
+            self.setup_editor()
+
     def setup_url(self):
         urllib.request.urlretrieve(
             self.url, str(self.path_config.joinpath(self.file_config)))
+
+    def setup_editor(self):
+        self.config['text_editor'] = self.fields.editor
+        File.write(
+            content=self.config,
+            path_file=str(self.path_config.joinpath(self.file_config)))
